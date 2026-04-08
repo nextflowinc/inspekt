da8e489a757cc070bdfa2039959afc27eff7e519
nextflowinc <next.flow.inc@gmail.com>
2026-04-08 01:00:00 -0400

    Second commit

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-export { default } from "next-auth/middleware";
-
-// On définit les routes qui nécessitent d'être connecté
-export const config = { 
-  matcher: [
-    "/dashboard/:path*", // Bloque tout ce qui commence par /dashboard
-    // Tu pourras ajouter d'autres routes privées ici plus tard
-  ] 
-};
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* tes autres options ici si tu en as */
 };
 
 export default nextConfig;
