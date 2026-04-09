b84a0bef03c6ddb93972026d32aba29b6c9688a7
nextflowinc <next.flow.inc@gmail.com>
2026-04-08 20:37:42 -0400

    fix config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,5 @@
-import { defineConfig, env } from 'prisma/config';
+import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
-  datasource: {
-    url: env('DATABASE_URL')
-  }
+  datasourceUrl: process.env.DATABASE_URL,
 });
